e28e5d5f9d2850eadda3ef8770ae88a2cc048d06
Kevin MacMartin <prurigro@gmail.com>
2015-10-21 22:49:06 -0400

    Use https for the favicon

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -5,7 +5,7 @@
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="shortcut icon" href="http://archstrike.org/favicon.ico" />
+        <link rel="shortcut icon" href="https://archstrike.org/favicon.ico" />
         <script src="{{ elixir('js/lib.js') }}"></script>
         <script src="{{ elixir('js/app.js') }}"></script>
         @if (Config::get('app.debug'))
