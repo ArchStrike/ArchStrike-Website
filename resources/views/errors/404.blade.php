462dad48de9685e88e9700956ee40959f9fa03ca
Kevin MacMartin <prurigro@gmail.com>
2015-10-25 16:28:25 -0400

    Center the content of error pages

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -3,8 +3,8 @@
 @section('page')
 
 <div class="container page-container">
-    <div class="row">
-        <div class="col-xs-12 col-md-10 col-md-offset-1 column">
+    <div class="row error-row">
+        <div class="col-xs-12 col-md-10 error-column">
             <div class="page-wrapper">
                 <a href="/" title="Home"><img src="/img/archstrike.svg" class="img-responsive small-logo" /></a>
                 <div class="error">Page not found</div>
