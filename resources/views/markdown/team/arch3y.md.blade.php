b2c08ca69eea8d6268e132583f2a42f7af8f0548
Kevin MacMartin <prurigro@gmail.com>
2015-10-25 01:12:39 -0400

    Fix the link to arch3y's github profile

--- resources/views/markdown/team/arch3y.md.blade.php
+++ resources/views/markdown/team/arch3y.md.blade.php
@@ -2,6 +2,6 @@
 
 **email**: [arch3y@archstrike.org](mailto:arch3y@archstrike.org)
 
-**github**: [https://github.com/orgs/ArchStrike/people/archey](https://github.com/orgs/ArchStrike/people/archey)
+**github**: [https://github.com/archey](https://github.com/archey)
 
 **pgp fingerprint**: `E676 0D17 90A0 00E3 D89D  5ACA DF72 09F3 B84C 1B2F`
