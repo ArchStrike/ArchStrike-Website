3584951e5272726ff664c599075c0debb62c71ed
Kevin MacMartin <prurigro@gmail.com>
2015-10-25 22:42:37 -0400

    Throw "ArchStrike Security Distribution" on the left side of the footer and the copyright on the right side

--- resources/views/elements/footer.blade.php
+++ resources/views/elements/footer.blade.php
@@ -1,3 +1,4 @@
 <footer>
-    Copyright © 2015 The ArchStrike Team
+    <div class="footer-left">ArchStrike Security Distribution</div>
+    <div class="footer-right">Copyright © 2015 The ArchStrike Team</div>
 </footer>
