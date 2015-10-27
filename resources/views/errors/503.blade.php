7dc7c66b516af91ff7be2b982e425711b2113481
Kevin MacMartin <prurigro@gmail.com>
2015-10-26 21:49:51 -0400

    Capitalize the letters of the 404 message and create a proper 503 error page

--- resources/views/errors/503.blade.php
+++ resources/views/errors/503.blade.php
@@ -1,16 +1,25 @@
-@extends('main')
-
-@section('page')
-
-<div class="container page-container">
-    <div class="row error-row">
-        <div class="col-xs-12 col-md-10 error-column">
-            <div class="page-wrapper">
-                <a href="/" title="Home"><img src="/img/archstrike.svg" class="img-responsive small-logo" /></a>
-                <div class="error">Be right back</div>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>ArchStrike: Be Right Back</title>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="shortcut icon" href="https://archstrike.org/favicon.ico" />
+        <link rel="stylesheet" href="{{ elixir('css/app.css') }}">
+    </head>
+    <body>
+        <div id="page-content">
+            <div class="container page-container">
+                <div class="row error-row">
+                    <div class="col-xs-12 col-md-10 error-column">
+                        <div class="page-wrapper">
+                            <a href="/" title="Home"><img src="/img/archstrike.svg" class="img-responsive small-logo" /></a>
+                            <div class="error">Be Right Back</div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
-    </div>
-</div>
-
-@endsection
+    </body>
+</html>
