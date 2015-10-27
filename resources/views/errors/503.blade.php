4b7d1919179d889fd01909a105f0f7efefec3a87
Kevin MacMartin <prurigro@gmail.com>
2015-10-26 21:38:43 -0400

    Replace the 503 page with one based on the 404 error page

--- resources/views/errors/503.blade.php
+++ resources/views/errors/503.blade.php
@@ -1,47 +1,16 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>Be right back.</title>
+@extends('main')
 
-        <link href="//fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+@section('page')
 
-        <style>
-            html, body {
-                height: 100%;
-            }
-
-            body {
-                margin: 0;
-                padding: 0;
-                width: 100%;
-                color: #B0BEC5;
-                display: table;
-                font-weight: 100;
-                font-family: 'Lato';
-            }
-
-            .container {
-                text-align: center;
-                display: table-cell;
-                vertical-align: middle;
-            }
-
-            .content {
-                text-align: center;
-                display: inline-block;
-            }
-
-            .title {
-                font-size: 72px;
-                margin-bottom: 40px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="container">
-            <div class="content">
-                <div class="title">Be right back.</div>
+<div class="container page-container">
+    <div class="row error-row">
+        <div class="col-xs-12 col-md-10 error-column">
+            <div class="page-wrapper">
+                <a href="/" title="Home"><img src="/img/archstrike.svg" class="img-responsive small-logo" /></a>
+                <div class="error">Be right back</div>
             </div>
         </div>
-    </body>
-</html>
+    </div>
+</div>
+
+@endsection
