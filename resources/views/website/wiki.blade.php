30f77778aed710685facf81e5cee1df3fa0d85d7
Kevin MacMartin <prurigro@gmail.com>
2015-11-08 22:17:56 -0500

    Make the wiki breadcrumb background dark, the text bold, use an actual arrow to separate breadcrumb items rather than the greater than sign, make all wiki headings bold, and widen the wiki page

--- resources/views/website/wiki.blade.php
+++ resources/views/website/wiki.blade.php
@@ -4,19 +4,19 @@
 
 <div class="container page-container">
     <div class="row">
-        <div class="col-xs-12 col-md-10 col-md-offset-1 column">
+        <div class="col-xs-12 column">
             <h1>ArchStrike Wiki</h1>
             <div class="breadcrumb">
                 @if($page == 'index')
                     Home
                 @else
-                    <a href="/wiki">Home</a> &gt; {{ ucfirst($page) }}
+                    <a href="/wiki">Home</a> <span>&rarr;</span> {{ ucfirst($page) }}
                 @endif
             </div>
         </div>
     </div>
     <div class="row wiki-row">
-        <div class="col-xs-12 col-md-10 col-md-offset-1 column">
+        <div class="col-xs-12 column">
             @include("markdown.wiki.$page")
         </div>
     </div>
