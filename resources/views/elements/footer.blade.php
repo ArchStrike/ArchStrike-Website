b57a7da75901a21ee92854ae5d714d76cfa79dc2
xorond <igorv466@gmail.com>
2016-04-20 22:13:37 +0300

    Fixed year on footer

--- resources/views/elements/footer.blade.php
+++ resources/views/elements/footer.blade.php
@@ -1,4 +1,4 @@
 <footer>
     <div class="footer-left">ArchStrike</div>
-    <div class="footer-right">Copyright © 2015 The ArchStrike Team</div>
+    <div class="footer-right">Copyright © 2016 The ArchStrike Team</div>
 </footer>
