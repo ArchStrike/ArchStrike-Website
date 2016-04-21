e8f5df1273a775cac07ddc37a56ddb9bf2701a6e
xorond <igorv466@gmail.com>
2016-04-22 02:18:36 +0300

    Added new wiki section on index

--- resources/views/markdown/wiki/index.md.blade.php
+++ resources/views/markdown/wiki/index.md.blade.php
@@ -7,3 +7,5 @@ Welcome to the [ArchStrike Wiki](/wiki): your source for ArchStrike documentatio
 [Setup ArchStrike](/wiki/setup) - How to add the **ArchStrike** repository to Arch Linux.
 
 [Repositories](/wiki/repositories) - A rundown of each of the **ArchStrike** repositories.
+
+[Testing PKGBUILDs](/wiki/chroot) - A quick guide on putting together your chroot environment to test PKGBUILD files on.
