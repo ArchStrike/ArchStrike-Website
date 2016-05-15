919c7b145f6f43f3aba34e2a6287bbd6455ad53b
Kevin MacMartin <prurigro@gmail.com>
2016-05-15 17:04:46 -0400

    Remove the written Copyright next to the copyright symbol

--- resources/views/elements/footer.blade.php
+++ resources/views/elements/footer.blade.php
@@ -1,4 +1,4 @@
 <footer>
     <div class="footer-left">ArchStrike</div>
-    <div class="footer-right">Copyright © {{ date('Y') }} The ArchStrike Team</div>
+    <div class="footer-right">&copy; {{ date('Y') }} The ArchStrike Team</div>
 </footer>
