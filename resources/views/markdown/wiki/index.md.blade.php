c0482cefb05dc9029d33f95d69a830a2bdac23e8
Kevin MacMartin <prurigro@gmail.com>
2016-05-30 15:26:34 -0400

    Add link to the metasploit wiki page

--- resources/views/markdown/wiki/index.md.blade.php
+++ resources/views/markdown/wiki/index.md.blade.php
@@ -2,10 +2,14 @@
 
 Welcome to the [ArchStrike Wiki](/wiki): your source for ArchStrike documentation.
 
-## Sections
+## ArchStrike
 
 [Setup ArchStrike](/wiki/setup) - How to add the **ArchStrike** repository to Arch Linux.
 
 [Repositories](/wiki/repositories) - A rundown of each of the **ArchStrike** repositories.
 
 [Testing PKGBUILDs](/wiki/chroot) - A quick guide on putting together your chroot environment to test PKGBUILD files on.
+
+## Guides
+
+[Metasploit](/wiki/msf) - Information on getting metasploit setup
