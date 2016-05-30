4600bcfbd51532bf0d3c795351ac12c80c9208a2
Kevin MacMartin <prurigro@gmail.com>
2016-05-30 15:23:45 -0400

    Add languages and inline-highlight some files

--- resources/views/markdown/wiki/msf.md.blade.php
+++ resources/views/markdown/wiki/msf.md.blade.php
@@ -6,21 +6,21 @@ In order to configure the database for Metasploit Framework, you'll need to crea
 
 If it is the first time you are running postgres, make sure to initialize the database cluster first.
 
-```
+```bash
 sudo -i -u postgres
 ```
 
-to become the postgres user, and then run
+to become the postgres user, and then run:
 
-```
+```bash
 initdb --locale $LANG -E UTF8 -D '/var/lib/postgres/data'
 ```
 
 as the postgres user to achieve it.
 
-Then switch to the root user and start and enable the postgresql service with
+Then switch to the root user and start and enable the postgresql service with:
 
-```
+```bash
 systemctl start postgresql
 systemctl enable postgresql
 ```
@@ -31,7 +31,7 @@ Now that postgresql is running, it is time to create our database for MSF.
 
 Run the following commands as the postgres user to create your database
 
-```
+```bash
 createuser msfdbuser -P -S -R -D
 createdb -O msfdbuser msfdb
 exit
@@ -39,7 +39,7 @@ exit
 
 ## Create and configure database.yml for MSF to use
 
-```
+```conf
 production:
 adapter: "postgresql"
 database: "msfdb"
@@ -53,21 +53,21 @@ timeout: 5
 
 Save this file to `/usr/share/metasploit/database.yml` and make sure you change the CHANGEME password to the one you specified while creating the database.
 
-Now set the `MSF_DATABASE_CONFIG` environment variable so that it points to your database.yml
+Now set the `MSF_DATABASE_CONFIG` environment variable so that it points to your `database.yml`
 
-```
+```bash
 sudo echo 'export MSF_DATABASE_CONFIG=/usr/share/metasploit/database.yml' > /etc/profile.d/msf.sh
 chmod 755 /etc/profile.d/msf.sh
 ```
 
-Then source msf.sh or log out and back in to your system.
+Then source `msf.sh` or log out and back in to your system.
 
-```
+```bash
 source /etc/profile.d/msf.sh
 ```
 
-And finally start msfconsole. It will detect if database.yml exists and connect to the postgresql database.
+And finally start msfconsole. It will detect if `database.yml` exists and connect to the postgresql database.
 
-```
+```bash
 msfconsole
 ```
