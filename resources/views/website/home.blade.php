c81fe82834ad1636facd6ca00597e86b0f412cda
Kevin MacMartin <prurigro@gmail.com>
2016-05-31 18:59:43 -0400

    Tweak the wording of the extended home page introduction and add some styling

--- resources/views/website/home.blade.php
+++ resources/views/website/home.blade.php
@@ -6,13 +6,20 @@
             <div class="intro-column">
                 <img src="/img/archstrike.svg" class="img-responsive home-logo" />
 
-                <div class="intro">
-                    A security layer for <a href="https://www.archlinux.org" target="_blank" rel="noopener noreferrer">Arch Linux</a> done the <a href="https://wiki.archlinux.org/index.php/Arch_Linux#Principles" target="_blank" rel="noopener noreferrer">Arch Way</a><br />
-                    and optimized for i686, x86_64, ARMv6, and ARMv7
+                <div class="container">
+                    <div class="row">
+                        <div class="col-xs-12 col-md-8 col-md-offset-2">
+                            <div class="intro">
+                                <p class="intro-description">
+                                    A security layer for <a href="https://www.archlinux.org" target="_blank" rel="noopener noreferrer">Arch Linux</a> done the <a href="https://wiki.archlinux.org/index.php/Arch_Linux#Principles" target="_blank" rel="noopener noreferrer">Arch Way</a><br />
+                                    and optimized for i686, x86_64, ARMv6, and ARMv7
+                                </p>
+
+                                <p class="intro-information">We follow the Arch Linux standards closely in order to keep our packages clean, proper and easy to maintain. Our team works hard to maintain the repository and give the best ArchStrike experience. If you find any issues, please don't hesitate to contact us via GitHub, IRC, Twitter or email. Any feedback is appreciated.</p>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-		<div class="info">
-		    We follow the Arch Linux standarts very closely in order to keep our packages clean, proper and easy to maintain. Our team is working very hard to maintain the repository and give you the best ArchStrike experience. If you find any issues, please don't hesitate to contact us via github, IRC, twitter or email. Any feedback is appreciated.
-		</div>
             </div>
         </div>
 
