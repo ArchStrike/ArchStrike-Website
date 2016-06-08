bdf2890ade226d4e9fd986c7a99acb9c36e78b89
xorond <xorond@archstrike.org>
2016-06-08 21:50:02 +0300

    changed msf entry name to metasploit

--- resources/views/markdown/wiki/index.md.blade.php
+++ resources/views/markdown/wiki/index.md.blade.php
@@ -11,6 +11,6 @@ Welcome to the [ArchStrike Wiki](/wiki): your source for ArchStrike documentatio
 
 ## Guides
 
-[Configuring the database for MSF](/wiki/msf) - A guide explaining the process of setting up the PostgreSQL database for Metasploit Framework on Arch Linux.
+[Configuring the database for MSF](/wiki/metasploit) - A guide explaining the process of setting up the PostgreSQL database for Metasploit Framework on Arch Linux.
 
 [Testing PKGBUILDs](/wiki/chroot) - A quick guide on putting together your chroot environment to test PKGBUILD files on.
