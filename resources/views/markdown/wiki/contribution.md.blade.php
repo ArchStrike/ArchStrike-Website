d51cbd617d4a8758ab514a3b43af6c39aa4471f3
xorond <xorond@archstrike.org>
2016-06-09 20:39:55 +0300

    added wiki entry regarding contributing

---
+++ resources/views/markdown/wiki/contribution.md.blade.php
@@ -0,0 +1,23 @@
+# Guide for getting involved in ArchStrike
+
+ArchStrike is a non-profit organization aiming to create a simple and clean penetration testing distribution for security researchers and enthusiasts. The project is being ran by volunteers. We are a small team, so we appreciate any kind of help being contributed. There are many ways you can contribute to ArchStrike. Here are just a few of them. 
+
+
+## Testing packages
+
+ArchStrike consists of two repositories which are `archstrike` and `archstrike-testing`.
+
+You can learn more about the repositories in our [wiki entry](https://archstrike.org/wiki/repositories). You can also study the full list of our [packages](https://archstrike.org/packages).
+
+Testing a package involves installing our `archstrike-testing` repository on your computer. Testing the packages helps us move them into the main repository in which they will be regularly updated and maintained by our team.
+
+After you inform us, we will confirm if the package works or not and it will be moved into the main repository.
+
+You can notify us via our github [issue tracker](https://github.com/ArchStrike/ArchStrike/issues) or any other communication medium you find suitable.
+
+## Submitting PKGBUILDs
+
+Submitting PKGBUILDs is a good way of helping out. The packages that fail to build can be seen on our [build issues page](https://archstrike.org/builder) and are prioritised for fixing. You can also create new PKGBUILDs for new tools that you want to see added in our repositories. If you are a developer, we are usually more then happy to add in new packages so you can submit them this way.
+
+You can find a more detailed guide about this on our [wiki entry](https://archstrike.org/wiki/chroot).
+
