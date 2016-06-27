eab98f62ca3051c3b1d8e09788842c1f81a0952b
Kevin MacMartin <prurigro@gmail.com>
2016-06-27 01:53:30 -0400

    Add both teal and black versions of each social SVG, create an IRC SVG in both black and teal, add the IRC SVG to the list of social media icons, create a new blade containing only the social media icons and include that in the footer, also include the list of social media icons in the intro area using the black SVGs in place of the link to the IRC

--- resources/views/elements/footer.blade.php
+++ resources/views/elements/footer.blade.php
@@ -1,9 +1,4 @@
 <footer>
     <div class="footer-left">&copy; {{ date('Y') }} The ArchStrike Team</div>
-
-    <div class="footer-right">
-        <a class="github" href="https://github.com/ArchStrike" title="ArchStrike Github" target="_blank" rel="noopener noreferrer"></a>
-        <a class="twitter" href="https://twitter.com/ArchStrike" title="ArchStrike Twitter" target="_blank" rel="noopener noreferrer"></a>
-        <a class="email" href="mailto:team@archstrike.org" title="ArchStrike Email"></a>
-    </div>
+    <div class="footer-right">@include('sections.social')</div>
 </footer>
---
+++ resources/views/sections/social.blade.php
@@ -0,0 +1,4 @@
+<a class="social-icon github" href="https://github.com/ArchStrike" title="Github" target="_blank" rel="noopener noreferrer"></a>
+<a class="social-icon twitter" href="https://twitter.com/ArchStrike" title="Twitter" target="_blank" rel="noopener noreferrer"></a>
+<a class="social-icon email" href="mailto:team@archstrike.org" title="Email"></a>
+<a class="social-icon irc" href="irc://chat.freenode.net/archstrike" title="IRC"></a>
