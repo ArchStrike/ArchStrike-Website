b051d2322e737d759056d686eb26b860e2100da3
Kevin MacMartin <prurigro@gmail.com>
2016-06-28 00:31:05 -0400

    Show two team members per row once there's enough space and always center team members horizontally

--- resources/views/website/team.blade.php
+++ resources/views/website/team.blade.php
@@ -3,20 +3,24 @@
 @section('page')
     <div class="container page-container">
         <div class="row">
-            <div class="col-xs-12 col-md-10 col-md-offset-1 column">
+            <div class="col-xs-12">
                 <h1>Team</h1>
             </div>
         </div>
 
-        <div class="team-list">
-            @foreach([ 'arch3y', 'd1rt', 'prurigro', 'xorond' ] as $member)
-                <div class="member-row row">
-                    <div class="col-xs-12 col-md-10 col-md-offset-1 column">
-                        <div class="profile-picture" style="background-image: url('/img/team/{{ $member }}.jpg')"></div>
-                        <div class="profile-info">@include("markdown.team.$member")</div>
+        <div class="row">
+            <div class="team-list">
+                @foreach([ 'arch3y', 'd1rt', 'prurigro', 'xorond' ] as $member)
+                    <div class="col-xs-12 col-md-6 column">
+                        <div class="member-row">
+                            <div class="team-member">
+                                <div class="profile-picture" style="background-image: url(/img/team/{{ $member }}.jpg)"></div>
+                                <div class="profile-info">@include("markdown.team.$member")</div>
+                            </div>
+                        </div>
                     </div>
-                </div>
-            @endforeach
+                @endforeach
+            </div>
         </div>
     </div>
 @endsection
