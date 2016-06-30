244f9a313f82827f5c953a84ea679724829b24b0
Kevin MacMartin <prurigro@gmail.com>
2016-06-29 23:03:50 -0400

    Set news item page descriptions based on their news body text

--- resources/views/elements/news-page.blade.php
+++ resources/views/elements/news-page.blade.php
@@ -1,4 +1,6 @@
+@set('body', Parsedown::instance()->setMarkupEscaped(true)->parse($body))
 {{ Head::setTitle('News: ' . $title) }}
+{{ Head::setDescription(trim(preg_replace('/\s+/', ' ', strip_tags($body)))) }}
 <h1 class="news-title">{{ $title }}</h1>
 <div class="news-date">{{ $date }}</div>
-<div class="news-body">{!! Parsedown::instance()->setMarkupEscaped(true)->parse($body) !!}</div>
+<div class="news-body">{!! $body !!}</div>
