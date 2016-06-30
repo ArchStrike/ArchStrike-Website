b30be24770f698d113d46bdfb82670b7353d3a73
Kevin MacMartin <prurigro@gmail.com>
2016-06-29 23:56:00 -0400

    Position on the team page a bit better

--- resources/views/website/team.blade.php
+++ resources/views/website/team.blade.php
@@ -3,7 +3,7 @@
 @section('page')
     <div class="container page-container">
         <div class="row">
-            <div class="col-xs-12">
+            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-12 col-md-offset-0">
                 <h1>Team</h1>
             </div>
         </div>
@@ -13,10 +13,8 @@
                 @foreach([ 'arch3y', 'd1rt', 'prurigro', 'xorond' ] as $member)
                     <div class="col-xs-12 col-md-6 column">
                         <div class="member-row">
-                            <div class="team-member">
-                                <div class="profile-picture" style="background-image: url(/img/team/{{ $member }}.jpg)"></div>
-                                <div class="profile-info">@include("markdown.team.$member")</div>
-                            </div>
+                            <div class="profile-picture" style="background-image: url(/img/team/{{ $member }}.jpg)"></div>
+                            <div class="profile-info">@include("markdown.team.$member")</div>
                         </div>
                     </div>
                 @endforeach
