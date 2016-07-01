364dbb3ce2dd71f6969960c7eb92ed17e3679e03
xorond <xorond@archstrike.org>
2016-07-01 22:03:56 +0300

    future link for tutorials

--- resources/views/markdown/wiki/index.md.blade.php
+++ resources/views/markdown/wiki/index.md.blade.php
@@ -19,8 +19,5 @@ Welcome to the [ArchStrike Wiki](/wiki): your source for ArchStrike documentatio
 [Installing NVIDIA and CUDA for cudahashcat](/wiki/cuda) - A guide explaining how to install the NVIDIA drivers and CUDA runtime libraries necessary for cudahashcat to work.
 
 [Testing PKGBUILDs](/wiki/chroot) - A quick guide on putting together your chroot environment to test PKGBUILD files on.
-<<<<<<< d80439a9fb7273d502a220639d0b9178a3ab99e3
-=======
 
 ## [Tutorials](/wiki/tutorials-index)
->>>>>>> add future link for tutorials
