a15297dd27f3cce1458bb77bc9ddc68f4f6c1016
xorond <xorond@archstrike.org>
2016-07-08 03:24:34 +0300

    populate page with sample stuff

--- resources/views/markdown/downloads/index.md.blade.php
+++ resources/views/markdown/downloads/index.md.blade.php
@@ -1,1 +1,18 @@
-[ISO](https://archstrike.org) ArchStrike Openbox ISO
+## Release Info
+The image can be burned to a CD, mounted as an ISO file, or be directly written to a USB stick using a utility like `dd`.
+
+* **Current Release:** Version goes here
+* **ISO Size:** Size goes here
+* [Installation Guide](https://archstrike.org/wiki/setup)
+
+## HTTP Direct Download
+[Direct Link](direct link here)
+[Direct Link 2](other mirrors etc.)
+
+**Checksums**
+* MD5: `hash here`
+* SHA512: `hash here`
+
+## BitTorrent Download (Recommended)
+[Magnet Link](magnet link here)
+[Torrent File](torrent link here)
