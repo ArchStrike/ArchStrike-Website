8403c1c1677437e6a768fe94b6d302e29223cc79
xorond <xorond@archstrike.org>
2016-07-22 19:57:52 +0300

    activate downloads page

--- resources/views/markdown/downloads/index.md.blade.php
+++ resources/views/markdown/downloads/index.md.blade.php
@@ -1,24 +1,76 @@
-## Release Info
+### Release Info
 
 The image can be burned to a CD, mounted as an ISO file, or be directly written to a USB stick using a utility like `dd`.
 
-* **Current Release:** Version goes here
-* **ISO Size:** Size goes here
+* **Current Release:** 2016.07.21
+* **ISO Size (x86_64):** 4.0G
+* **ISO Size (i686):** 3.8G
 * [Installation Guide](https://archstrike.org/wiki/setup)
 
-## HTTP Direct Download
+### Direct Download Links
 
-[Direct Link](https://archstrike.org)
+## x86_64 (64 bit)
 
-[Direct Link 2](https://archstrike.org)
+**Official Mirrors**
+
+[United States](https://mirror.archstrike.org/os/ArchStrike-openbox-2016.07.21-x86_64.iso)
+
+[United Kingdom](https://uk.mirror.archstrike.org/os/ArchStrike-openbox-2016.07.21-x86_64.iso)
+
+[Japan](https://jp.mirror.archstrike.org/os/ArchStrike-openbox-2016.07.21-x86_64.iso)
+
+**Community Mirrors**
+
+[KeyStealth](http://mirror.keystealth.org/os/ArchStrike-openbox-2016.07.21-x86_64.iso)
+
+[OuterWorld](http://archstrike.outerworld.org/os/ArchStrike-openbox-2016.07.21-x86_64.iso)
+
+[i3d](http://mirror.i3d.net/pub/archstrike/os/ArchStrike-openbox-2016.07.21-x86_64.iso)
+
+[Wh1t3Fox](https://mirror.wh1t3fox.net/archstrike/os/ArchStrike-openbox-2016.07.21-x86_64.iso)
+
+[JMU](http://mirror.jmu.edu/pub/archstrike/os/ArchStrike-openbox-2016.07.21-x86_64.iso)
+
+[MirrorService](https://www.mirrorservice.org/sites/mirror.archstrike.org/os/ArchStrike-openbox-2016.07.21-x86_64.iso)
+
+## i686 (32 bit)
+
+**Official ArchStrike Mirrors**
+
+[United States](https://mirror.archstrike.org/os/ArchStrike-openbox-2016.07.21-i686.iso)
+
+[United Kingdom](https://uk.mirror.archstrike.org/os/ArchStrike-openbox-2016.07.21-i686.iso)
+
+[Japan](https://jp.mirror.archstrike.org/os/ArchStrike-openbox-2016.07.21-i686.iso)
+
+**Community Mirrors**
+
+[KeyStealth](http://mirror.keystealth.org/os/ArchStrike-openbox-2016.07.21-i686.iso)
+
+[OuterWorld](http://archstrike.outerworld.org/os/ArchStrike-openbox-2016.07.21-i686.iso)
+
+[i3d](http://mirror.i3d.net/pub/archstrike/os/ArchStrike-openbox-2016.07.21-i686.iso)
+
+[Wh1t3Fox](https://mirror.wh1t3fox.net/archstrike/os/ArchStrike-openbox-2016.07.21-i686.iso)
+
+[JMU](http://mirror.jmu.edu/pub/archstrike/os/ArchStrike-openbox-2016.07.21-i686.iso)
+
+[MirrorService](https://www.mirrorservice.org/sites/mirror.archstrike.org/os/ArchStrike-openbox-2016.07.21-i686.iso)
 
 ### Checksums
 
-* MD5: `hash here`
-* SHA512: `hash here`
+## ArchStrike-openbox-2016.07.21-x86_64.iso
 
-## BitTorrent Download (Recommended)
+* SHA256: `7c96ff6dda00bb913df635746494955e4b7c8b850002f09023f608437bfc06f1`
+* SHA512: `64f337bfec9dcd0532b20e0130d2fb5069e87d87fee4692163b102086232fc7a3c02482cd0cc73ba6a05846f7e1617826dbdfe9cbaca31ae9b516ac0cf8903f9`
 
-[Magnet Link](https://archstrike.org)
+## ArchStrike-openbox-2016.07.21-i686.iso
 
-[Torrent File](https://archstrike.org)
+* SHA256: `6fe730044de3cdbe0d9ac520efbb000bcd1f786325d436d48f1953fc747477ff`
+* SHA512: `fbd568f3572c256431f020963fd095e95d7bbdbc30dc28c6f5f4645c22d7e607aaaec5be5f103a6005b3779415ce1a0e51806cd81ecdb5747ebeb78f14030a3a`
+
+###  Torrent Download (Recommended)
+
+[x86_64](https://mirror.archstrike.org/os/ArchStrike-openbox-2016.07.21-x86_64.iso.torrent)
+
+[i686](https://mirror.archstrike.org/os/ArchStrike-openbox-2016.07.21-x86_64.iso.torrent)
