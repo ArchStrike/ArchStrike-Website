9296a00854ab70b899bfb37630e6bdc441d0de64
xorond <xorond@archstrike.org>
2016-07-24 21:41:55 +0300

    make it look a bit better

--- resources/views/markdown/downloads/index.md.blade.php
+++ resources/views/markdown/downloads/index.md.blade.php
@@ -4,13 +4,13 @@
 
 The image can be burned to a CD, mounted as an ISO file, or be directly written to a USB stick using a utility like `dd`.
 
-* **Current Release:** 2016.07.21
-* **ISO Size (x86_64):** 4.0G
-* **ISO Size (i686):** 3.8G
-* [Installation Guide](https://archstrike.org/wiki/setup)
+**Current Release:** 2016.07.21
+**ISO Size (x86_64):** 4.0G
+**ISO Size (i686):** 3.8G
+[Installation Guide](https://archstrike.org/wiki/setup)
 
-* To log in as root, `root` for the **username and the password**
-* To log in as standard user, `archstrike` for the **username and the password**
+* To log in as root, use `root` for the **username and the password**
+* To log in as standard user, use `archstrike` for the **username and the password**
 
 ##  Torrent Download (Recommended)
 
