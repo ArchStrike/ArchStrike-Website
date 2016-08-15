cdc621c1c96261c8265b4be7df92ec58475b30e9
Kevin MacMartin <prurigro@gmail.com>
2016-08-15 00:53:59 -0400

    Use the piwik tracking code exactly as provided

--- resources/views/tracking/piwik.blade.php
+++ resources/views/tracking/piwik.blade.php
@@ -1,17 +1,15 @@
-{{-- Piwik --}}
+<!-- Piwik -->
 <script type="text/javascript">
-    var _paq = _paq || [];
-    _paq.push([ "trackPageView" ]);
-    _paq.push([ "enableLinkTracking" ]);
-
-    (function() {
-        var u = "https://stats.archstrike.org";
-        _paq.push([ "setTrackerUrl", u + "piwik.php" ]);
-        _paq.push([ "setSiteId", "1" ]);
-        var d = document, g = d.createElement("script"), s = d.getElementsByTagName("script")[0];
-        g.type = "text/javascript"; g.async = true; g.defer = true; g.src = u + "piwik.js"; s.parentNode.insertBefore(g, s);
-    })();
+  var _paq = _paq || [];
+  _paq.push(['trackPageView']);
+  _paq.push(['enableLinkTracking']);
+  (function() {
+    var u="//stats.archstrike.org/";
+    _paq.push(['setTrackerUrl', u+'piwik.php']);
+    _paq.push(['setSiteId', '1']);
+    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+    g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
+  })();
 </script>
-
-<noscript><p><img src="https://stats.archstrike.org/piwik.php?idsite=1" style="border:0;" alt="" /></p></noscript>
-{{-- End Piwik Code --}}
+<noscript><p><img src="//stats.archstrike.org/piwik.php?idsite=1" style="border:0;" alt="" /></p></noscript>
+<!-- End Piwik Code -->
