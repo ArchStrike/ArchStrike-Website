8af31cc016dbc0f21b0dc43d3a4105a7cfbed33b
Kevin MacMartin <prurigro@gmail.com>
2016-08-15 00:34:06 -0400

    Skip a package when it doesn't exist on the home page package updates list rather than no longer writing the list

--- resources/views/elements/pkgupdates.blade.php
+++ resources/views/elements/pkgupdates.blade.php
@@ -1,19 +1,21 @@
 @set('pkg_count', 1)
 
 @foreach($pkgupdates as $update)
-    @if($pkg_count <= 6 && App\Abs::exists($update['pkgname']))
-        @set('pkg_count', $pkg_count + 1)
+    @if($pkg_count <= 6)
+        @if(App\Abs::exists($update['pkgname']))
+            @set('pkg_count', $pkg_count + 1)
 
-        <div class="sidebar-box-item">
-            <a href="/packages/{{ $update['pkgname'] }}">{{ $update['pkgname'] }}</a>
-            {{ $update['pkgver'] }}-{{ $update['pkgrel'] }}
+            <div class="sidebar-box-item">
+                <a href="/packages/{{ $update['pkgname'] }}">{{ $update['pkgname'] }}</a>
+                {{ $update['pkgver'] }}-{{ $update['pkgrel'] }}
 
-            @if($update['info'] == 1)
-                <span class="info">(new)</span>
-            @endif
+                @if($update['info'] == 1)
+                    <span class="info">(new)</span>
+                @endif
 
-            {{-- <span class="date">{{ $update['date'] }}</span> --}}
-        </div>
+                {{-- <span class="date">{{ $update['date'] }}</span> --}}
+            </div>
+        @endif
     @else
         @break
     @endif
