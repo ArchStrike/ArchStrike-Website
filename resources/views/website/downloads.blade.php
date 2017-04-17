8b8d86284a26ca81032208e989bb41f07ba65b85
Kevin MacMartin <prurigro@gmail.com>
2017-04-17 15:20:54 -0400

    Hide the minimal ISO on the downloads page as it hasn't been released yet

--- resources/views/website/downloads.blade.php
+++ resources/views/website/downloads.blade.php
@@ -29,11 +29,13 @@
                 <h3>Release Info</h3>
                 <p>The image can be burned to a CD, mounted as an ISO file, or be directly written to a USB stick using a utility like <code>dd</code>.</p>
 
+                {{--
                 <div class="space-above space-below">
                     <p><strong>{{ $iso_minimal_variant }} Release:</strong> {{ $iso_minimal_release }}</p>
                     <p><strong>ISO Size (x86_64):</strong> {{ env('ISO_MINIMAL_64_SIZE') }}</p>
                     <p><strong>ISO Size (i686):</strong> {{ env('ISO_MINIMAL_32_SIZE') }}</p>
                 </div>
+                --}}
 
                 <div class="space-below">
                     <p><strong>{{ $iso_variant }} Release:</strong> {{ $iso_release }}</p>
@@ -50,12 +52,14 @@
 
                 <h3>Torrent Download (Recommended)</h3>
 
+                {{--
                 <div class="space-above">
                     <ul>
                         <li><a href="{{ $torrent_mirror }}/os/{{ $iso_minimal64 }}.torrent">{{ $iso_minimal_name }} (x86_64)</a></li>
                         <li><a href="{{ $torrent_mirror }}/os/{{ $iso_minimal32 }}.torrent">{{ $iso_minimal_name }} (i686)</a></li>
                     </ul>
                 </div>
+                --}}
 
                 <div class="space-above">
                     <ul>
@@ -70,8 +74,10 @@
                     <form>
                         <select id="downloads-form-iso">
                             <option value="">Select an ISO</option>
+                            {{--
                             <option value="{{ $iso_minimal64 }}">{{ $iso_minimal_name }} (64 bit)</option>
                             <option value="{{ $iso_minimal32 }}">{{ $iso_minimal_name }} (32 bit)</option>
+                            --}}
                             <option value="{{ $iso64 }}">{{ $iso_name }} (64 bit)</option>
                             <option value="{{ $iso32 }}">{{ $iso_name }} (32 bit)</option>
                         </select>
@@ -95,6 +101,7 @@
                 <noscript>
                     <h3>Direct Download Links</h3>
 
+                    {{--
                     <h4>{{ $iso_minimal_name }} x86_64 (64 bit)</h4>
                     <h5>Official Mirrors</h5>
 
@@ -128,6 +135,7 @@
                             <li><a href="{{ $mirror['url'] }}os/{{ $iso_minimal32 }}">{{ $mirror['name'] }}</a></li>
                         @endforeach
                     </ul>
+                    --}}
 
                     <h4>{{ $iso_name }} x86_64 (64 bit)</h4>
                     <h5>Official Mirrors</h5>
@@ -166,6 +174,7 @@
 
                 <h3>Checksums</h3>
 
+                {{--
                 <div class="space-above space-below">
                     <h4>{{ $iso_minimal64 }}</h4>
                     <p class="checksum"><strong>SHA256:</strong> <input class="click-select" type="text" value="{{ env('ISO_MINIMAL_64_256') }}" readonly /></p>
@@ -177,6 +186,7 @@
                     <p class="checksum"><strong>SHA256:</strong> <input class="click-select" type="text" value="{{ env('ISO_MINIMAL_32_256') }}" readonly /></p>
                     <p class="checksum"><strong>SHA512:</strong> <input class="click-select" type="text" value="{{ env('ISO_MINIMAL_32_512') }}" readonly /></p>
                 </div>
+                --}}
 
                 <div class="space-below">
                     <h4>{{ $iso64 }}</h4>
