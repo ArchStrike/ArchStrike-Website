95f4aa58006ace86c9592cca337b904b48a91a2a
Kevin MacMartin <prurigro@gmail.com>
2017-09-05 22:22:40 -0400

    Move Craig West to the former team members section

--- app/Team.php
+++ app/Team.php
@@ -45,7 +45,7 @@ class Team {
             'email'  => 'wh1t3fox@archstrike.org',
             'github' => 'https://github.com/wh1t3fox',
             'pgp'    => '2684 FFAF ACA8 A554 9C54 DA90 7DDB 5DE1 2A3F 5213',
-            'status' => true
+            'status' => false
         ],
 
         [
